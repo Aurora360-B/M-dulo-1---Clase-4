568daba5333d372ca6cf57b034ac3227652cc382
Aurora <auroraortiz002@gmail.com>
2026-03-08 02:10:42 -0500

    Proyecto clase 4

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Registrando datos de Carlos en el sistema...");
+        System.out.println("Registrando datos de Ana en el sistema...");
+        System.out.println("Registrando datos de Aurora en el sistema...");
+        System.out.println("Registrando datos de Lina en el sistema...");
+        /*Se están repitiendo acciones, las acciones es oslo una línea pero cuando se vuelva mas compleja 
+        entonces se vuelve muy dificil, ejemplo si son 10 lineas se repitierían 4 veces,  */
+
+
+
+
+
+
+
+
+
+
+    }
+}
